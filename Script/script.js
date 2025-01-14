c9858177838938b23dc0127a557898b14259827e
mateus andrade <mateusandrade092@gmail.com>
2025-01-13 21:42:43 -0300

    inicio da interação JS na página

--- Script/script.js
+++ Script/script.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", (load) =>{
     console.log("DOM Fully load and parsed")
 });
+
+document.getElementById('about_me_button').addEventListener('click', function() {
+    window.location.hash = 'about_me';
+    document.getElementById('about_me').scrollIntoView();
+});
+
+document.getElementById('skills_button').addEventListener('click', function(){
+    window.location.hash = 'skills';
+    document.getElementById('skills').scrollIntoView();
+})
