6dc44c8d67a9878d2bae6985633cc479448fc980
mateus andrade <mateusandrade092@gmail.com>
2025-02-09 20:10:16 -0300

    alterações front end: script de controle de animação para as habilidades adicionado, alterado o hover dos dispositivos moveis, animação do nome e identificação acrescentada.

---
+++ Script/script.js
@@ -0,0 +1,31 @@
+const frontEnd = document.querySelector('.icones_de_habilidades_front-end');
+const backEnd = document.querySelector('.icones_de_habilidades_back-end');
+
+const habilidadesFront = frontEnd.querySelectorAll('.habilidade_em_html, .habilidade_em_css, .habilidade_em_js');
+const iconesFront = frontEnd.querySelectorAll('.icone_HTML, .icone_css, .icone_js');
+const barrasFront = frontEnd.querySelectorAll('.barra_carregamento_html, .barra_carregamento_css, .barra_carregamento_js');
+
+const habilidadesBack = backEnd.querySelectorAll('.habilidade_em_python, .habilidade_em_mysql');
+const iconesBack = backEnd.querySelectorAll('.icone_python, .icone_mysql');
+const barrasBack = backEnd.querySelectorAll('.barra_carregamento_python, .barra_carregamento_mysql');
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      // Animações para o Front-End
+      habilidadesFront.forEach(hab => hab.style.animation = 'surgir_esquerda 5s forwards');
+      iconesFront.forEach(icon => icon.style.animation = 'surgir_esquerda 3s forwards');
+      barrasFront.forEach(barra => barra.style.animation = 'surgir_direita 8s forwards');
+      
+      // Animações para o Back-End
+      habilidadesBack.forEach(hab => hab.style.animation = 'surgir_esquerda 5s forwards');
+      iconesBack.forEach(icon => icon.style.animation = 'surgir_esquerda 3s forwards');
+      barrasBack.forEach(barra => barra.style.animation = 'surgir_direita 8s forwards');
+      
+      observer.disconnect();
+    }
+  });
+}, { threshold: 0.5 });
+
+observer.observe(frontEnd);
+observer.observe(backEnd);
